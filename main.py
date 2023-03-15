e398084a42af22045d68945811a964c1c76630fd
ArcadeFortune <lamaa@bzz.ch>
2023-03-15 09:27:11 +0100

    detect which champion is picked

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
 from lcu_driver import Connector
+import requests
+import json
+
+#pip install lcu-driver
 
 connector = Connector()
 
@@ -30,8 +34,26 @@ def pasteFiles(champion):
     #TODO copy the keybinds from a db or something into the PersistedSettings.json file
     print(f'copying keybinds for champion Nr°{champion}')
     
-def whichChampionIs(champion):
-    return "Yoshino"
-    
+def whichChampionIs(champion_id):
+    # Get the latest version of Data Dragon
+    versions_url = "https://ddragon.leagueoflegends.com/api/versions.json"
+    response = requests.get(versions_url)
+    versions = json.loads(response.content)
+    latest_version = versions[0]
+
+    # Get the champion data for the latest version
+    url = f"http://ddragon.leagueoflegends.com/cdn/{latest_version}/data/en_US/champion.json"
+    response = requests.get(url)
+    champions_data = json.loads(response.content)["data"]
+
+    # Loop through the champions data and find the champion with the matching ID
+    for champion in champions_data.values():
+        if int(champion["key"]) == champion_id:
+            return champion["name"]
+
+    # Return an error message if no champion with the matching ID was found
+    return f"Error: Champion name not found for ID {champion_id}"
+
+# Example usage
 connector.start()
 
