0860ace399ec48916214dde646004ef8a5629601
ArcadeFortune <alessionights@gmail.com>
2023-03-17 19:52:14 +0100

    copies files with correct champion name

--- main.py
+++ main.py
@@ -5,35 +5,67 @@ import json
 #pip install lcu-driver
 
 connector = Connector()
+currentChampion = "nothing right now"
 
 
 @connector.ready
 async def connect(connection):
     print('watch Date A Live')
-    # print(connection.address)
-    # print(connection.auth_key)
+    print(connection.address)
+    print(connection.auth_key)
     
 @connector.close
 async def disconnect(_):
     print('The client has closed!')
     
-#everytime your League session UPDATEs:
-@connector.ws.register('/lol-champ-select/v1/session', event_types=('UPDATE',))
+#everytime your League session UPDATEs: 
+@connector.ws.register('/lol-gameflow/v1/gameflow-phase', event_types=('UPDATE',))
+async def main(connection, event):
+    await checkLockIn(connection, event.data)
+    await checkPostGame(connection, event.data)
+        
 async def checkLockIn(connection, event):
-    champion = await connection.request('get', '/lol-champ-select/v1/current-champion')
-    if champion.status != 200:
-        return
-    
-    champion = await champion.json()
-    if champion > 0:
-        #TODO find out who the champion Nr° ___ is
-        champion = whichChampionIs(champion)
-        pasteFiles(champion)
-
-def pasteFiles(champion):
+    if event == "GameStart":
+        
+        #when the game starts:
+        await updateChampion(connection)
+        pasteFiles()
+        
+async def checkPostGame(connection, event):
+    if event == 'EndOfGame':
+        
+        #when the game ends:
+        copyFiles()
+            
+        
+def pasteFiles():
+    print(f'copying keybinds for champion Nr°{currentChampion}')
     #TODO copy the keybinds from a db or something into the PersistedSettings.json file
-    print(f'copying keybinds for champion Nr°{champion}')
     
+def copyFiles():
+    with open('PersistedSettings.json', 'r') as f:
+        data = json.load(f)
+    
+    for file in data['files']:
+        if file['name'] == 'Input.ini':
+            sections = {}
+            for section in file['sections']:
+                sections[section['name']] = section['settings']
+            
+            output_data = {"name": "Input.ini", "sections": []}
+            for section_name in ['GameEvents', 'HUDEvents', 'Quickbinds', 'ShopEvents']:
+                if section_name in sections:
+                    output_data['sections'].append({"name": section_name, "settings": sections[section_name]})
+            
+            with open(f'{currentChampion}_settings.otto', 'w') as output_file:
+                json.dump(output_data, output_file, indent=4)
+                
+            break
+    else:
+        print("Could not find section 'Input.ini' in file 'PersistedSettings.json'.")
+    
+    
+############################### functions to clean everything ###############################
 def whichChampionIs(champion_id):
     # Get the latest version of Data Dragon
     versions_url = "https://ddragon.leagueoflegends.com/api/versions.json"
@@ -54,6 +86,18 @@ def whichChampionIs(champion_id):
     # Return an error message if no champion with the matching ID was found
     return f"Error: Champion name not found for ID {champion_id}"
 
-# Example usage
+async def updateChampion(connection):    
+    champion = await connection.request('get', '/lol-champ-select/v1/current-champion')
+    if champion.status != 200:
+        return
+    
+    champion = await champion.json()
+    champion = whichChampionIs(champion)
+        
+    #update currentChampion
+    global currentChampion
+    currentChampion = champion
+    print(f'updated current champion to: {currentChampion} / {champion}')
+    
 connector.start()
 
